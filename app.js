9476b524259860157c6d653f39f19b0bfe012d46
Arsham Khoee <arsham.gh97@gmail.com>
2022-12-23 20:54:31 +0330

    fixing bugs

--- app.js
+++ app.js
@@ -1,38 +1,5 @@
 import bodyParser from 'body-parser'
 import express, { request } from 'express'
-import prisma from './prisma'
-import { AppRoutes } from './routes'
-import auth from './middleware/auth'
 
-const app = express()
+export const app = express()
 app.use(bodyParser.json())
-
-async function main() {
-    await prisma.$connect()
-}
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-})
-  
-console.log(`Prisma ${prisma._clientVersion} is connected.`)
-
-AppRoutes.forEach(route => {
-    const controller = (request, response, next) => {
-        route.action(request, response)
-            .then(() => next)
-            .catch(err => next(err))
-    }
-    let args = [route.path, auth, controller]
-    if(!route.auth) {
-        args.splice(1, 1)
-    }
-    app[route.method](...args)
-})
-
-app.listen(3000, () => console.log('Server is running on port 3000'))
---
+++ index.js
@@ -0,0 +1,34 @@
+import { prisma } from './prisma'
+import { app } from './app'
+import { AppRoutes } from './routes'
+import { auth } from './middleware/auth'
+
+async function main() {
+    await prisma.$connect()
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+})
+  
+console.log(`Prisma ${prisma._clientVersion} is connected.`)
+
+AppRoutes.forEach(route => {
+    const controller = (request, response, next) => {
+        route.action(request, response)
+            .then(() => next)
+            .catch(err => next(err))
+    }
+    let args = [route.path, auth, controller]
+    if(!route.auth) {
+        args.splice(1, 1)
+    }
+    app[route.method](...args)
+})
+
+app.listen(3000, () => console.log('Server is running on port 3000'))
