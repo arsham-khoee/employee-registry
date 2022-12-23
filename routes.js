40caa8a045bdb232e95ca751c7ba7b10919bd700
Arsham Khoee <arsham.gh97@gmail.com>
2022-12-24 02:26:24 +0330

    designing all require routes

--- routes.js
+++ routes.js
@@ -11,4 +11,54 @@ export const AppRoutes = [
         method: 'post',
         auth: false,
     },
+    {
+        path: '/employees',
+        method: 'get',
+        auth: true,
+    },
+    {
+        path: '/employees',
+        method: 'post',
+        auth: true,
+    },
+    {
+        path: '/employees/:id',
+        method: 'get',
+        auth: true,
+    },
+    {
+        path: '/employees/:id',
+        method: 'patch',
+        auth: true,
+    },
+    {
+        path: '/employees/:id',
+        method: 'delete',
+        auth: true,
+    },
+    {
+        path: '/departments',
+        method: 'get',
+        auth: true,
+    },
+    {
+        path: '/departments/:id',
+        method: 'get',
+        auth: true,
+    },
+    {
+        path: '/departments',
+        method: 'post',
+        auth: true,
+    },
+    {
+        path: '/departments/:id',
+        method: 'patch',
+        auth: true,
+    },
+    {
+        path: '/departments/:id',
+        method: 'delete',
+        auth: true,
+    },
 ]
