9968fb10ec46d90f5fcccbb9f5280321fb127179
Arsham Khoee <arsham.gh97@gmail.com>
2022-12-23 21:37:19 +0330

    make previsou and current department arbitrary

---
+++ routes.js
@@ -0,0 +1,14 @@
+
+
+export const AppRoutes = [
+    {
+        path: "/signup",
+        method: "post",
+        auth: false,
+    },
+    {
+        path: '/login',
+        method: 'post',
+        auth: false,
+    },
+]
